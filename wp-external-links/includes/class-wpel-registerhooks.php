901bfb13397f221dd67bd3e368e8a1398c7e5c2f
Victor Villaverde Laan <info@freelancephp.net>
2016-06-03 16:57:40 +0200

    activation and uninstall

--- wp-external-links/includes/class-wpel-registerhooks.php
+++ wp-external-links/includes/class-wpel-registerhooks.php
@@ -35,24 +35,45 @@ final class WPEL_Registerhooks extends WPRun_Base_1x0x0
     protected function activate( $networkwide )
     {
         global $wpdb;
-        error_log( 'WPEL Demo Activate!' );
-        $sites = wp_get_sites();
         
         if ( is_multisite() && $networkwide ) {
             // network activation
+            $sites = wp_get_sites();
             $active_blog = $wpdb->blogid;
 
             foreach ( $sites as $site ) {
                 switch_to_blog( $site[ 'blog_id' ] );
-//                $this->activate_site();
+                $this->activate_site();
             }
 
             // switch back to active blog
             switch_to_blog( $active_blog );
+
+            $this->activate_network();
         } else {
             // single site activation
-//            $this->activate_site();
+            $this->activate_site();
         }
+    }
+
+    /**
+     * Activate network
+     * @return void
+     */
+    private function activate_network()
+    {
+        $network_already_set = get_site_option( 'wpel-network-settings' );
+
+        if ( $network_already_set ) {
+            return;
+        }
+
+        // network default settings
+        $network_values = WPEL_Network_Fields::get_instance()->get_default_values();
+        $network_admin_values = WPEL_Network_Admin_Fields::get_instance()->get_default_values();
+
+        update_site_option( 'wpel-network-settings', $network_values );
+        update_site_option( 'wpel-network-admin-settings', $network_admin_values );
     }
 
     /**
@@ -61,104 +82,136 @@ final class WPEL_Registerhooks extends WPRun_Base_1x0x0
      */
     private function activate_site()
     {
-        // convert old to new db option values
-        // check for old option values version < 2.0.0
-        $old_meta = get_option( 'wp_external_links-meta' );
-        $old_main = get_option( 'wp_external_links-main' );
+        $site_already_set = get_option( 'wpel-external-link-settings' );
 
-        // stop when old settings not exist
-        if ( empty( $old_meta ) && empty( $old_main ) ) {
+        if ( $site_already_set ) {
             return;
         }
+        
+        // get default values
+        $external_link_values = WPEL_External_Link_Fields::get_instance()->get_default_values();
+        $internal_link_values = WPEL_Internal_Link_Fields::get_instance()->get_default_values();
+        $exceptions_link_values = WPEL_Exceptions_Fields::get_instance()->get_default_values();
+        $admin_link_values = WPEL_Admin_Fields::get_instance()->get_default_values();
 
-        // get other old values
-        $old_seo = get_option( 'wp_external_links-seo' );
-        $old_style = get_option( 'wp_external_links-style' );
-        $old_extra = get_option( 'wp_external_links-extra' );
-        $old_screen = get_option( 'wp_external_links-screen' );
+        // check for old option values version < 2.0.0
+        $old_main = get_option( 'wp_external_links-main' );
 
-        $external_link_fields = WPEL_External_Link_Fields::get_instance();
-        $external_link_values = $external_link_fields->get_option_values();
-
-        $internal_link_fields = WPEL_Internal_Link_Fields::get_instance();
-        $internal_link_values = $internal_link_fields->get_option_values();
-
-        $exceptions_fields = WPEL_Internal_Link_Fields::get_instance();
-        $exceptions_link_values = $exceptions_fields->get_option_values();
-
-        $admin_fields = WPEL_Admin_Fields::get_instance();
-        $admin_link_values = $admin_fields->get_option_values();
-
-        // mapping
+        // convert old to new db option values
         if ( ! empty( $old_main ) ) {
-            $external_link_values[ 'target' ] = str_replace( '_none', '_self', $old_main[ 'filter_page' ] );
-            $exceptions_link_values[ 'apply_all' ] = (string) $old_main[ 'filter_page' ];
-            $exceptions_link_values[ 'apply_post_content' ] = (string) $old_main[ 'filter_posts' ];
-            $exceptions_link_values[ 'apply_comments' ] = (string) $old_main[ 'filter_comments' ];
-            $exceptions_link_values[ 'apply_widgets' ] = (string) $old_main[ 'filter_widgets' ];
-            $exceptions_link_values[ 'exclude_urls' ] = (string) $old_main[ 'ignore' ];
-            $exceptions_link_values[ 'subdomains_as_internal_links' ] = (string) $old_main[ 'ignore_subdomains' ];
-        }
-        if ( ! empty( $old_seo ) ) {
-            $external_link_values[ 'rel_follow' ] = ( 1 == $old_seo[ 'nofollow' ] ) ? 'nofollow' : 'follow';
-            $external_link_values[ 'rel_follow_overwrite' ] = (string) $old_seo[ 'overwrite_follow' ];
-            $external_link_values[ 'rel_external' ] = (string) $old_seo[ 'external' ];
-            $external_link_values[ 'title' ] = str_replace( '%title%', '{title}', $old_seo[ 'title' ] );
-        }
-        if ( ! empty( $old_style ) ) {
-            if ( $old_style[ 'icon' ] ) {
-                $external_link_values[ 'icon_type' ] = 'image';
-                $external_link_values[ 'icon_image' ] = $old_style[ 'icon' ];
+            // get other old values
+            $old_meta = get_option( 'wp_external_links-meta' );
+            $old_seo = get_option( 'wp_external_links-seo' );
+            $old_style = get_option( 'wp_external_links-style' );
+            $old_extra = get_option( 'wp_external_links-extra' );
+            $old_screen = get_option( 'wp_external_links-screen' );
+
+            // helper function
+            $val = function ( $arr, $key, $default = '' ) {
+                if ( ! isset( $arr[ $key ] ) ) {
+                    return $default;
+                }
+
+                return (string) $arr[ $key ];
+            };
+
+            // mapping
+            if ( ! empty( $old_main ) ) {
+                $target = $val( $old_main, 'target' );
+                $external_link_values[ 'target' ] = str_replace( '_none', '_self', $target );
+
+                $exceptions_link_values[ 'apply_all' ] = $val( $old_main, 'filter_page' );
+                $exceptions_link_values[ 'apply_post_content' ] = $val( $old_main, 'filter_posts' );
+                $exceptions_link_values[ 'apply_comments' ] = $val( $old_main, 'filter_comments' );
+                $exceptions_link_values[ 'apply_widgets' ] = $val( $old_main, 'filter_widgets' );
+                $exceptions_link_values[ 'exclude_urls' ] = $val( $old_main, 'ignore' );
+                $exceptions_link_values[ 'subdomains_as_internal_links' ] = $val( $old_main, 'ignore_subdomains' );
             }
-            $external_link_values[ 'class' ] = $old_style[ 'class_name' ];
-            $external_link_values[ 'no_icon_for_img' ] = (string) $old_style[ 'image_no_icon' ];
-        }
-        if ( ! empty( $old_extra ) ) {
-            $exceptions_link_values[ 'ignore_script_tags' ] = (string) $old_style[ 'fix_js' ];
-        }
-        if ( ! empty( $old_screen ) ) {
-            $admin_link_values[ 'own_admin_menu' ] = ( 'admin.php' == $old_screen[ 'menu_position' ] ) ? '1' : '0';
+            if ( ! empty( $old_seo ) ) {
+                $external_link_values[ 'rel_follow' ] = ( '1' ==  $val( $old_seo, 'nofollow' ) ) ? 'nofollow' : 'follow';
+                $external_link_values[ 'rel_follow_overwrite' ] = $val( $old_seo, 'overwrite_follow' );
+                $external_link_values[ 'rel_external' ] = $val( $old_seo, 'external' );
+
+                $title = $val( $old_seo, 'title' );
+                $external_link_values[ 'title' ] = str_replace( '%title%', '{title}', $title );
+            }
+            if ( ! empty( $old_style ) ) {
+                if ( $old_style[ 'icon' ] ) {
+                    $external_link_values[ 'icon_type' ] = 'image';
+                    $external_link_values[ 'icon_image' ] = $val( $old_style, 'icon', '1' );
+                }
+                $external_link_values[ 'class' ] = $val( $old_style, 'class_name' );
+                $external_link_values[ 'no_icon_for_img' ] = $val( $old_style, 'image_no_icon' );
+            }
+            if ( ! empty( $old_extra ) ) {
+                // nothing
+            }
+            if ( ! empty( $old_screen ) ) {
+                $admin_link_values[ 'own_admin_menu' ] = ( 'admin.php' == $val( $old_screen, 'menu_position' ) ) ? '1' : '';
+            }
+
+            // delete old values
+            delete_option( 'wp_external_links-meta' );
+            delete_option( 'wp_external_links-main' );
+            delete_option( 'wp_external_links-seo' );
+            delete_option( 'wp_external_links-style' );
+            delete_option( 'wp_external_links-extra' );
+            delete_option( 'wp_external_links-screen' );
         }
 
         // update new values
-        $external_link_fields->update_option_values( $external_link_values );
-        $internal_link_fields->update_option_values( $internal_link_values );
-        $exceptions_fields->update_option_values( $exceptions_link_values );
-        $admin_fields->update_option_values( $admin_link_values );
+        update_option( 'wpel-external-link-settings', $external_link_values );
+        update_option( 'wpel-internal-link-settings', $internal_link_values );
+        update_option( 'wpel-exceptions-settings', $exceptions_link_values );
+        update_option( 'wpel-admin-settings', $admin_link_values );
 
         // update meta data
         $plugin_data = get_plugin_data( WPEL_Plugin::get_plugin_file() );
         update_option( 'wpel-version', $plugin_data[ 'Version' ] );
         update_option( 'wpel-show-notice', true );
+    }
 
-        // delete old values
-        delete_option( 'wp_external_links-meta' );
-        delete_option( 'wp_external_links-main' );
-        delete_option( 'wp_external_links-seo' );
-        delete_option( 'wp_external_links-style' );
-        delete_option( 'wp_external_links-extra' );
-        delete_option( 'wp_external_links-screen' );
+    /**
+     * Uninstall site
+     */
+    protected function uninstall()
+    {
+        global $wpdb;
+
+        if ( is_multisite() ) {
+            // network activation
+            $sites = wp_get_sites();
+            $active_blog = $wpdb->blogid;
+            foreach ( $sites as $site ) {
+                switch_to_blog( $site[ 'blog_id' ] );
+                $this->uninstall_site();
+            }
+
+            // switch back to active blog
+            switch_to_blog( $active_blog );
+
+            // network settings
+            delete_site_option( 'wpel-network-settings' );
+            delete_site_option( 'wpel-network-admin-settings' );
+        } else {
+            // single site activation
+            $this->activate_site();
+        }
     }
 
     /**
      * Plugin uninstall procedure
      */
-    protected function uninstall()
+    protected function uninstall_site()
     {
-        error_log( 'WPEL Demo Uninstall!' );
-
         // delete options
-        $external_link_fields = WPEL_External_Link_Fields::get_instance();
-        $external_link_values = $external_link_fields->delete_option_values();
+        delete_option( 'wpel-external-link-settings' );
+        delete_option( 'wpel-internal-link-settings' );
+        delete_option( 'wpel-exceptions-settings' );
+        delete_option( 'wpel-admin-settings' );
 
-        $internal_link_fields = WPEL_Internal_Link_Fields::get_instance();
-        $internal_link_values = $internal_link_fields->delete_option_values();
-
-        $exceptions_fields = WPEL_Internal_Link_Fields::get_instance();
-        $exceptions_link_values = $exceptions_fields->delete_option_values();
-
-        $admin_fields = WPEL_Admin_Fields::get_instance();
-        $admin_link_values = $admin_fields->delete_option_values();
+        delete_option( 'wpel-version' );
+        delete_option( 'wpel-show-notice' );
     }
 
 }
