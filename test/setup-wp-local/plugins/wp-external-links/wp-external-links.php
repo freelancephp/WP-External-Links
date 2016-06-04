1f39e3d7ad4e52cc8815c23d0ee67fced02e338d
Victor Villaverde Laan <info@freelancephp.net>
2016-06-04 04:41:17 +0200

    update local setup

--- test/setup-wp-local/plugins/wp-external-links/wp-external-links.php
+++ test/setup-wp-local/plugins/wp-external-links/wp-external-links.php
@@ -8,7 +8,7 @@ Version: 1000
 Author URI: http://www.freelancephp.net
 License: Dual licensed under the MIT and GPL licenses
 */
-define('WP_EXTERNAL_LINKS_FILE', __FILE__);
+define('TEST_WPEL_PLUGIN_FILE', __FILE__);
 
 // include plugin
 require_once($_SERVER['DOCUMENT_ROOT'] . 'git/WP-External-Links/wp-external-links/wp-external-links.php');
