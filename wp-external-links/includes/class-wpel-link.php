5e2de6a6dc26e29f9ee85a14fc5d3e55d8cf1378
Victor Villaverde Laan <info@freelancephp.net>
2016-06-05 13:17:40 +0200

    set version 2.0.2

--- wp-external-links/includes/class-wpel-link.php
+++ wp-external-links/includes/class-wpel-link.php
@@ -7,7 +7,7 @@
  *
  * @package  WPEL
  * @category WordPress Plugin
- * @version  2.0.1
+ * @version  2.0.2
  * @author   Victor Villaverde Laan
  * @link     http://www.finewebdev.com
  * @link     https://github.com/freelancephp/WP-External-Links
